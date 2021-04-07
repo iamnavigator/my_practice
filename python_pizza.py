2ec40145519c739f8ea4dd2cc4fccd823b7b5c3f
iamnavigator <78704125+iamnavigator@users.noreply.github.com>
2021-04-07 20:56:16 +0100

    Update python_pizza.py
    order pizza

--- python_pizza.py
+++ python_pizza.py
@@ -1,24 +1,36 @@
-print("welcome to Python Pizza")
-size = input("which size of pizza would you like s,m or l: ").lower()
-if size == "s":
+print("Welcome to PythonPizza")
+bill = 0
+pizza = input("Which pizza size would you like to order: 's', 'm', 'l' \n").lower()
+if pizza == "s":
     bill = 15
-
-elif size == "m":
-    bill = 20
-
-else:
-    bill = 25
-
-add_pepperoni = input("would you like to add pepperoni: y or n\n").lower()
-if add_pepperoni == "y":
-    if size == "s":
-        bill += 2
+    pepperoni = input("Would you like to add pepperoni: 'y' or 'n' \n").lower()
+    if pepperoni == "y":
+        bill += 1
+    cheese = input("would you like extra cheese: 'y' or 'no' \n").lower()
+    if cheese == "y":
+        bill += 1
+        print(f"your final bill is {bill}")
     else:
+        print(f"your final bill is {bill}")
+elif pizza == "m":
+    bill = 20
+    pepperoni = input("Would you like to add pepperoni: 'y' or 'n' \n").lower()
+    if pepperoni == "y":
         bill += 3
-    
-extra_cheese = input("would you like to add extra cheese: y or n\n").lower()
-if extra_cheese == "y":
-    bill += 1
-    print(f"please pay {bill}")
-else:
-    print(f"please pay {bill}")
+    cheese = input("would you like extra cheese: 'y' or 'n' \n").lower()
+    if cheese == "y":
+        bill += 1
+        print(f"your final bill is {bill}")
+    else:
+        print(f"your final bill is {bill}")
+elif pizza == "l":
+    bill = 25
+    pepperoni = input("Would you like to add pepperoni: 'y' or 'n' \n").lower()
+    if pepperoni == "y":
+        bill += 3
+    cheese = input("would you like extra cheese: 'y' or 'n' \n").lower()
+    if cheese == "y":
+        bill += 1
+        print(f"your final bill is {bill}")
+    else:
+        print(f"your final bill is {bill}")
